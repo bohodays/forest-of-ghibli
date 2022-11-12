2742481c94a776bf628e04f0b69ea88dc417c753
bohodays <bohodays0316@gamil.com>
2022-11-13 00:57:08 +0900

    UPDATE: Navbar, Main Page

---
+++ img_crawling.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "final_pjt.settings")
+import django
+django.setup()
+from movies.models import Movie
+
+
+def img_crawling():
+    req = requests.get('https://movie.naver.com/movie/bi/mi/photoView.naver?code=39640')
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    imgs = soup.select_one(
+        '#photo_area > div > div.img_src._img_area > div > div > div > img'
+        )
+    print(imgs)
+    print(imgs.get("src"))
+
+
+    return
+
+if __name__=='__main__':
+    img_crawling()
+
+
--- static/movies/js/main.js
+++ static/movies/js/main.js
@@ -21,3 +21,41 @@ document.addEventListener('scroll', () => {
 arrowUp.addEventListener('click', () => {
   document.body.scrollIntoView({ behavior:'smooth' });
 })
+
+const searchMark = document.querySelector('.fa-magnifying-glass');
+const xMark = document.querySelector('.fa-xmark');
+let menuItems = document.querySelectorAll('.menu__item');
+const navbarMenu = document.querySelector('.navbar__menu');
+const hiddenMenus = document.querySelectorAll('.menu__hidden');
+
+
+searchMark.addEventListener('click', () => {
+  menuItems.forEach((item) => {
+    item.classList.add('menu-invisible');
+    item.style.transform = 'translateY(-10px)';
+  })
+  
+  hiddenMenus.forEach((itme) => {
+    itme.style.opacity = '1';
+    itme.style.display = 'block';
+    itme.style.pointerEvents = 'all';
+  })
+  
+  searchMark.style.transform = 'translate(-370px, 2px)';
+  // createSearchInput();
+})
+
+xMark.addEventListener('click', () => {
+  menuItems.forEach((item) => {
+    item.classList.remove('menu-invisible');
+    item.style.transform = 'translateY(0px)';
+  })
+
+  hiddenMenus.forEach((itme) => {
+    itme.style.opacity = '0';
+    itme.style.display = 'absolute';
+    itme.style.pointerEvents = 'none';
+  })
+
+  searchMark.style.transform = 'translate(0px, 0px)';
+})
