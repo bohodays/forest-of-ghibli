c9b196314b5f246a11cb29ef14fa45e95a371528
ehus964 <ehus964@gmail.com>
2022-11-17 10:09:47 +0900

    create js

--- static/movies/js/detail.js
+++ static/movies/js/detail.js
@@ -37,37 +37,28 @@ forms.forEach((form) => {
 
 
 // 댓글 삭제
-const deleteForms = document.querySelector('.delete-forms')
-if (deleteForms){
+const deleteForms = document.querySelectorAll('.delete-forms')
+deleteForms.forEach((deleteForm) => {
+  deleteForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const movieId = event.target.dataset.movieId
+    const deleteId = event.target.dataset.deleteId
 
-    deleteForms.addEventListener('submit', function (event) {
-      event.preventDefault()
-      console.log('delete eventTargetDataset',event.target.dataset)
-      
-      const movieId = event.target.dataset.movieId
-      // 이름만 delete고 지울 댓글의 아이디를 가져옴
-      const deleteId = event.target.dataset.deleteId
-       
-      axios({
-        method: 'post',
-        url: `http://127.0.0.1:8000/movies/${movieId}/comments/${deleteId}/delete/`,
-        headers: {'X-CSRFToken': csrftoken,},
-      })
-      .then((response) => {
-        // console.log(response.data, 'delete axios then function')
-        
-        // url 요청을 가져오는걸 성공 했을 경우 여기서 
-        const isDeleted = response.data.is_deleted
-        console.log('thenthenthenthen',isDeleted);
-        
-        // return window.location.reload()
-        // likeBtn.value = isLiked ? '좋아요 취소' : '좋아요'
-      })
-      .catch((error) => {
-        console.log('??',error.response)
-      })
+axios({
+  method: 'post',
+  url: `http://127.0.0.1:8000/movies/${movieId}/comments/${deleteId}/delete/`,
+  headers: {'X-CSRFToken': csrftoken,},
+})
+  .then((response) => {
+    // 버튼을 누른 댓글
+    const userForm = event.target.parentNode.parentNode.parentNode.parentNode;
+    userForm.remove();
+  })
+    .catch((error) => {
+      console.log('??',error.response)
     })
-  }
+  })
+})
   
   
   // 댓글 생성
@@ -98,7 +89,18 @@ const createForms = document.querySelectorAll('.form-comment')
     }).then((response)=>{
       console.log(response.data.comment_content);
       console.log(response.data.comment_movie_rate);
-      
+      const responseContent = response.data.comment_content
+      const responseRate = response.data.comment_movie_rate
+      const reviewContent = document.createElement('p')
+      reviewContent.innerText=responseContent
+      const contentForm = document.querySelector('.user-comments__wrap')
+      contentForm.appendChild(reviewContent)
+      // const reviewContent = document.getElementsByName('content')[0]
+      // const reviewRate = document.getElementsByName('movie_rate')[0]
+      // reviewContent.innerText = responseContent
+      // reviewRate.innerText = responseRate
+      // console.log('review content', reviewContent);
+      // console.log('review rate', reviewRate);
 
     }).catch(error=>{
       console.log('??',error);
