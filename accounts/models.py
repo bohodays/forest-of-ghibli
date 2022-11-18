f496e40232c53c8422564ca83c46e2ff42ce4f5a
bohodays <bohodays0316@gmail.com>
2022-11-18 10:16:35 +0900

    update

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+def accounts_image_path(instance, filename):
+    print(instance)
+    return f'accounts/{filename}'
+# Create your models here.
+class User(AbstractUser):
+    nickName = models.CharField(max_length =20, unique = True, blank=False)
+    profileImg = models.ImageField(blank=True, upload_to=accounts_image_path)
+    
