3d69a151773ff5ba8372b385ae74f83e06e08676
ehgus964 <ehgus964@gmail.com>
2022-11-20 22:53:40 +0900

    profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,3 +14,5 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
         fields = ('nickName','profileImg')
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from movies.models import Movie
 
 def accounts_image_path(instance, filename):
     print(instance)
@@ -10,4 +10,5 @@ class User(AbstractUser):
     nickName = models.CharField(max_length =20, unique = True, blank=False)
     profileImg = models.ImageField(blank=True, upload_to=accounts_image_path)
     GBTI = models.TextField(blank=True)
-    
+    movie_title = models.CharField(max_length=20, blank=True)
+    movie_poster = models.TextField(blank=True)
